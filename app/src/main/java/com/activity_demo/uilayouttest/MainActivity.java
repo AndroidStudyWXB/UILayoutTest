f8f702bec2806e92387a12a21656cb1d81611e8e
WXB506 <1922686887@qq.com>
2016-03-14 23:21:51 +0800

    initial commit

---
+++ app/src/main/java/com/activity_demo/uilayouttest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.activity_demo.uilayouttest;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by WXB506 on 2016/2/23.
+ */
+public class MainActivity extends ActionBarActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
